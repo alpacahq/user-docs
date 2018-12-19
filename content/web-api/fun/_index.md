4dcaedeecd564bc64e171a5114d3aba474b4eb82
ttt733 <trevor.thackston@gmail.com>
2018-12-19 10:53:11 -0600

    Add initial progress on example code blocks

---
+++ content/web-api/fun/_index.md
@@ -0,0 +1,2 @@
+fun
+{{< code-example example-id="fun-test" path=.File.Path pathURL="/web-api/fun/examples/fun-test" >}}
---
+++ content/web-api/fun/examples/fun-test/ex1.js
@@ -0,0 +1,26 @@
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+const alpaca = new Alpaca({
+    keyId: 'REPLACEME',
+    secretKey: 'REPLACEME',
+    paper: true
+})
+
+// Submit a market order to buy 1 share of Apple at market price
+alpaca.createOrder(
+    symbol: 'AAPL',
+    qty: 1,
+    side: 'buy',
+    type: 'market',
+    time_in_force: 'day'
+)
+
+// Submit a limit order to attempt to sell 1 share of AMD at a
+// particular price ($20.50) when the market opens
+alpaca.createOrder(
+    symbol: 'AMD',
+    qty: 1,
+    side: 'sell',
+    type: 'limit',
+    time_in_force: 'opg',
+    limit_price: 20.50
+)
