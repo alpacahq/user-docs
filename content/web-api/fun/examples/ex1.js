7eb8396360d3f71f90b5ea6088526aaee8657dac
ttt733 <trevor.thackston@gmail.com>
2018-12-20 18:21:37 -0600

    Fix missing braces in JS example code

--- content/web-api/fun/examples/ex1.js
+++ content/web-api/fun/examples/ex1.js
@@ -6,21 +6,21 @@ const alpaca = new Alpaca({
 })
 
 // Submit a market order to buy 1 share of Apple at market price
-alpaca.createOrder(
+alpaca.createOrder({
     symbol: 'AAPL',
     qty: 1,
     side: 'buy',
     type: 'market',
     time_in_force: 'day'
-)
+})
 
 // Submit a limit order to attempt to sell 1 share of AMD at a
 // particular price ($20.50) when the market opens
-alpaca.createOrder(
+alpaca.createOrder({
     symbol: 'AMD',
     qty: 1,
     side: 'sell',
     type: 'limit',
     time_in_force: 'opg',
     limit_price: 20.50
-)
+})
