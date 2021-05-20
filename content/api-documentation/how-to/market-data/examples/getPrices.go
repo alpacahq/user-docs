ffe492684ab3dda2a716cd916e7ec4ef6e27d673
Sohajda Ábel <sohajdaabel@gmail.com>
2021-05-20 16:23:52 +0200

    Update examples

--- content/api-documentation/how-to/market-data/examples/getPrices.go
+++ content/api-documentation/how-to/market-data/examples/getPrices.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/alpacahq/alpaca-trade-api-go/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v2"
 )
 
 func init() {
-	alpaca.SetBaseUrl("https://paper-api.alpaca.markets")
+	alpaca.SetBaseUrl("https://data.alpaca.markets")
 }
 
 func main() {
-	// Get daily price data for AAPL over the last 5 trading days.
-	barCount := 5
-	bars, err := alpaca.GetSymbolBars("AAPL", alpaca.ListBarParams{
-		Timeframe: "day",
-		Limit: &barCount,
-	})
-	if err != nil {
-		panic(err)
+	bars := alpaca.GetBars(
+		"AAPL", v2.Day, v2.Raw, time.Now().Add(-7*24*time.Hour), time.Now().Add(-20*time.Minute), 7)
+	var barset []v2.Bar
+
+	for bar := range bars {
+		if bar.Error != nil {
+			panic(bar.Error)
+		}
+		barset = append(barset, bar.Bar)
 	}
 
 	// See how much AAPL moved in that timeframe.
-	startPrice := bars[0].Open
-	endPrice := bars[len(bars)-1].Close
-	percentChange := (endPrice - startPrice) / startPrice * 100
-	fmt.Printf("AAPL moved %v%% over the last 5 days.", percentChange)
+	startPrice := barset[0].Open
+	endPrice := barset[len(barset)-1].Close
+	percentChange := ((endPrice - startPrice) / startPrice ) * 100
+	fmt.Printf("AAPL moved %v%% over the last 7 days.", percentChange)
 }
--- content/api-documentation/how-to/market-data/examples/getPrices.js
+++ content/api-documentation/how-to/market-data/examples/getPrices.js
@@ -1,20 +1,25 @@
-const Alpaca = require('@alpacahq/alpaca-trade-api')
-const alpaca = new Alpaca()
+const moment = require("moment");
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+const alpaca = new Alpaca();
 
-// Get daily price data for AAPL over the last 5 trading days.
-const barset = alpaca.getBars(
-    'day',
-    'AAPL',
-    {
-        limit: 5
-    }
-).then((barset) => {
-    const aapl_bars = barset['AAPL']
+//Get daily price data for AAPL over the last 7 days.
+let bars = alpaca.getBarsV2(
+  "AAPL",
+  {
+    start: moment().subtract(7, "days").format(),
+    end: moment().subtract(20, "minutes").format(),
+    timeframe: "1Day",
+  },
+  alpaca.configuration
+);
+const barset = [];
 
-    // See how much AAPL moved in that timeframe.
-    const week_open = aapl_bars[0].o
-    const week_close = aapl_bars.slice(-1)[0].c
-    const percent_change = (week_close - week_open) / week_open * 100
+for await (let b of bars) {
+  barset.push(b);
+}
 
-    console.log(`AAPL moved ${percent_change}% over the last 5 days`)
-})
+const week_open = barset[0].OpenPrice;
+const week_close = barset.slice(-1)[0].ClosePrice;
+const percent_change = ((week_close - week_open) / week_open) * 100;
+
+console.log(`AAPL moved ${percent_change}% over the last 7 days`);
